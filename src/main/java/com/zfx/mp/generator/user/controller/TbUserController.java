14e75097523f5681468f5fb7490b31f2ffaa92df
zfx1101804091 <1875586562@qq.com>
2020-01-18 12:17:30 +0800

    mybatis-plus 代码自动生成工具 以及idea的mybatis mapper和xml之间跳转工具 mybatisX

---
+++ src/main/java/com/zfx/mp/generator/user/controller/TbUserController.java
@@ -0,0 +1,20 @@
+package com.zfx.mp.generator.user.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author zheng_fx
+ * @since 2020-01-18
+ */
+@Controller
+@RequestMapping("/user/tb-user")
+public class TbUserController {
+
+}
---
+++ src/main/java/com/zfx/mp/generator/user/service/impl/TbUserServiceImpl.java
@@ -0,0 +1,20 @@
+package com.zfx.mp.generator.user.service.impl;
+
+import com.zfx.mp.generator.user.entity.TbUser;
+import com.zfx.mp.generator.user.mapper.TbUserMapper;
+import com.zfx.mp.generator.user.service.ITbUserService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author zheng_fx
+ * @since 2020-01-18
+ */
+@Service
+public class TbUserServiceImpl extends ServiceImpl<TbUserMapper, TbUser> implements ITbUserService {
+
+}
